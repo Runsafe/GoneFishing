483b750c3d4dd4a3c56c43d41e1b460a85c242c8
Jesse Gauthier <Jessegauthier_070296@hotmail.com>
2014-06-17 18:02:53 -0400

    Added Flint Tickets, Mass Demand

---
+++ src/no/runsafe/gonefishing/scavenger/FlintTicket.java
@@ -0,0 +1,23 @@
+package no.runsafe.gonefishing.scavenger;
+
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+public class FlintTicket implements IFishingLootItem
+{
+	@Override
+	public RunsafeMeta getItem()
+	{
+		if (item == null)
+		{
+			item = Item.Miscellaneous.paper.getItem();
+			item.setAmount(1);
+			item.setDisplayName("§6Flint Tickets");
+			item.addLore("§7Offical currency!");
+			item.addLore("§cOffical Currency at Captain Flint's Reef");
+		}
+		return item;
+	}
+
+	private RunsafeMeta item;
+}
