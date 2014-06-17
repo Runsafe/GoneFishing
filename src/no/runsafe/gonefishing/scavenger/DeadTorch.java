4ab864bdd71675226a76c2e4b957caa2901b05f1
Jesse Gauthier <Jessegauthier_070296@hotmail.com>
2014-06-17 17:47:59 -0400

    Added the DeadTorch

---
+++ src/no/runsafe/gonefishing/scavenger/DeadTorch.java
@@ -0,0 +1,22 @@
+package no.runsafe.gonefishing.scavenger;
+
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+public class DeadTorch implements IFishingLootItem
+{
+	@Override
+	public RunsafeMeta getItem()
+	{
+		if (item == null)
+		{
+			item = Item.Material.TORCH.getItem();
+			item.setAmount(1);
+			item.setDisplayName("§cBurned out Torch");
+			item.addLore("§7The water put this torch out, looks like it won't light.");
+		}
+		return item;
+	}
+
+	private RunsafeMeta item;
+}
