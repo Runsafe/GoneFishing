33eab3d67c121d4aa1f856317545b58002d9cda6
Jesse Gauthier <Jessegauthier_070296@hotmail.com>
2014-06-17 18:14:37 -0400

    Added SeaWeed

---
+++ src/no/runsafe/gonefishing/scavenger/SeaWeed.java
@@ -0,0 +1,22 @@
+package no.runsafe.gonefishing.scavenger;
+
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+public class SeaWeed implements IFishingLootItem
+{
+	@Override
+	public RunsafeMeta getItem()
+	{
+		if (item == null)
+		{
+			item = Item.Decoration.vine.getItem();
+			item.setAmount(1);
+			item.setDisplayName("§aWashed up seaweed");
+			item.addLore("§7Eww this feels slimy.");
+		}
+		return item;
+	}
+
+	private RunsafeMeta item;
+}
