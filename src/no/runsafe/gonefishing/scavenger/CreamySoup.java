50ca80952597d68d6d55c9caa731392a03542e32
Jesse Gauthier <Jessegauthier_070296@hotmail.com>
2014-06-17 18:27:26 -0400

    Added Soup!

---
+++ src/no/runsafe/gonefishing/scavenger/CreamySoup.java
@@ -0,0 +1,22 @@
+package no.runsafe.gonefishing.scavenger;
+
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+public class CreamySoup implements IFishingLootItem
+{
+	@Override
+	public RunsafeMeta getItem()
+	{
+		if (item == null)
+		{
+			item = Item.Cooked.MUSHROOM_SOUP.getItem();
+			item.setAmount(1);
+			item.setDisplayName("§6Creamy's Nasty Outdated Soup");
+			item.addLore("§7You look down to see something moving in the soup.");
+		}
+		return item;
+	}
+
+	private RunsafeMeta item;
+}
