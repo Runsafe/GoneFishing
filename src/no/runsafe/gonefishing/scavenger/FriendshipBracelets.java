e54f799136b24eb9d40db729747cb1bc6132be04
Matthew Eddy <Backmatt@live.com>
2014-06-21 16:21:50 -0400

    Created new rare item- Set of friendship bracelets

---
+++ src/no/runsafe/gonefishing/scavenger/FriendshipBracelets.java
@@ -0,0 +1,23 @@
+package no.runsafe.gonefishing.scavenger;
+
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
+
+public class FriendshipBracelets implements IFishingLootItem
+{
+	@Override
+	public RunsafeMeta getItem()
+	{
+		if (item == null)
+		{
+			item = Item.Miscellaneous.Leash.getItem();
+			item.setAmount(2);
+			item.setDisplayName("§bTattered Friendship Bracelet");
+			item.addLore("§7One of a set of friendship bracelets found deep in the ocean.");
+			item.addlore("§dShare one with a friend! <3");
+		}
+		return item;
+	}
+
+	private RunsafeMeta item;
+}
