e3cf18f5110fee9e6e9e6ba75b89bb9b5ff10807
Centralan <jessegauthier070296@gmail.com>
2023-11-29 16:37:35 -0500

    Update MaxSkyBase.java

--- src/no/runsafe/gonefishing/scavenger/MaxSkyBase.java
+++ src/no/runsafe/gonefishing/scavenger/MaxSkyBase.java
@@ -4,18 +4,19 @@ import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 
 public class MaxSkyBase implements IFishingLootItem
+{
+        @Override
+        public RunsafeMeta getItem()
         {
-@Override
-public RunsafeMeta getItem()
-        {
-        if (item == null)
-        {
-        item = Block.Minecraft.Cobblestone.getItem();
-        item.setAmount(1);
-        item.setDisplayName("§cPiece of Max8abugs Skybase");
-        item.addLore("§3This must have washed up from a grief.");
-        }
-        return item;
+                if (item == null)
+                {
+                        item = Block.Minecraft.Cobblestone.getItem();
+                        item.setAmount(1);
+                        item.setDisplayName("§cPiece of Max8abugs Skybase");
+                        item.addLore("§3This must have washed up from a grief.");
+                }
+                return item;
         }
 
-private RunsafeMeta item;
+        private RunsafeMeta item;
+}
